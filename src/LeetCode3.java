18b8b7a8116333b8e33e410867b33555f1d1f3eb
yuhao_li <1416964247@qq.com>
2018-12-20 00:49:22 +0000

    LeetCode 3 complete

--- src/LeetCode3.java
+++ src/LeetCode3.java
@@ -12,9 +12,11 @@ public class LeetCode3 {
         Map<Character, Integer> map = new HashMap<>();
         for (int j = 0, i = 0; j < n; j++) {
             if (map.containsKey(s.charAt(j))) {
+                //因为新的value（字符index）覆盖了原先的，一般get()比i要新，通过Math.max()获取最大（新）的字符index
                 i = Math.max(map.get(s.charAt(j)), i);
             }
             ans = Math.max(ans, j - i + 1);
+            //相同的key，新的value覆盖原先的value
             map.put(s.charAt(j), j + 1);
         }
         return ans;
