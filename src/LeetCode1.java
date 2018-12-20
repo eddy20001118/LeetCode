c7e83604a73b5f1ed967d0e0f132ac5958e57715
yuhao_li <1416964247@qq.com>
2018-12-20 00:33:24 +0000

    LeetCode 1-3

---
+++ src/LeetCode1.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class LeetCode1 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        LeetCode1 leetCode1 = new LeetCode1();
+        System.out.println(Arrays.toString(leetCode1.twoSum(nums, target)));
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i] + nums[j];
+                if (sum == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    return answer;
+                }
+            }
+        }
+        throw new IllegalStateException("No solution");
+    }
+}
---
+++ src/LeetCode2.java
@@ -0,0 +1,41 @@
+public class LeetCode2 {
+    ListNode l1 = new ListNode(1);
+    ListNode l2 = new ListNode(0);
+
+    public LeetCode2() {
+        l1.next = new ListNode(8);
+    }
+
+    public static void main(String[] args) {
+        LeetCode2 leetCode2 = new LeetCode2();
+        leetCode2.addTwoNumbers(leetCode2.l1, leetCode2.l2);
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode answer = new ListNode(0);
+        ListNode a1 = l1, a2 = l2, temp = answer; //这里是引用赋值，所以temp发生变化后answer跟着变化
+        int curVal = 0;
+        while (a1 != null || a2 != null) {
+            int a1val = (a1 != null) ? a1.val : 0;
+            int a2val = (a2 != null) ? a2.val : 0;
+            int sum = curVal + a1val + a2val;
+            curVal = sum / 10; //curVal 是进位运算，所以要/10然后在下一次迭代中赋值给下一位
+            temp.next = new ListNode(sum % 10); //与10取模就是为了防止出现进位
+            temp = temp.next;
+            if(a1 != null) a1 = a1.next;
+            if(a2 != null) a2 = a2.next;
+        }
+        if (curVal > 0) {
+            temp.next = new ListNode(curVal);
+        }
+        return answer.next;
+    }
+}
